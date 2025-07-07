6a6c3ffe96d440fe9f4ca9d7e907bcf3792776a6
Fatos Ava <fatossava47@gmail.com>
2025-07-07 23:37:42 +0300

    Fix: use process.env.PORT for Render

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 // JSON dosyasını yükle
 const products = JSON.parse(fs.readFileSync('./products.json'));
